7a82ca7251843d4a373de62780810e7ebbed2d02
Ian Khrashchevskyi <39925631+iankhr@users.noreply.github.com>
2019-09-17 11:25:28 +0200

    Updated output

--- famamacbeth.py
+++ famamacbeth.py
@@ -9,10 +9,17 @@ The code bellow performs Fama-MacBeth regressions as described in
 Fama, F. & Macbeth, J.D. (2003) "Risk, Return and Equilibrium: Empirical Tests"
     Journal of Political Economy Vol 81, No 3, pp. 607-636
     
+Standard errors were checked against Petersen's simulated data for Standard Error
+testing:
+    https://www.kellogg.northwestern.edu/faculty/petersen/htm/papers/se/test_data.htm
 
+Petersen M. A. (2009) "Estimating Standard Errors in Finance Panel Data Sets:
+    Comparing Approaches", The Review of Financial Studies, Vol 22, Issue 1, 
+    pp 435-480
 """
 import numpy as np
 import pandas as pd
+import shutil
 
 class FamaMacBeth(object):
     def __init__(self, Y, X, add_constant = True, yLabel = None, xLabel = None):
@@ -32,8 +39,19 @@ class FamaMacBeth(object):
         self.output = None
         
         # write down name of Y and X
-        self.yLabel = yLabel
-        self.xLabel = xLabel
+        if yLabel is None:
+            self.yLabel = 'y'
+        else:
+            self.yLabel = yLabel
+        
+        if xLabel is None:
+            self.xLabel = xLabel
+        else:
+            # check whether it matches the length of X
+            if len(xLabel) == len(X):
+                self.xLabel = xLabel
+            else:
+                raise ValueError("Length of X labels does not match length of X")
         
     
     
@@ -41,7 +59,8 @@ class FamaMacBeth(object):
         """
         The function performs Fama-MacBeth regression as in the paper and then
         presents the output.
-        """ 
+        """
+        self._NW = NW
         try:              
             if lags<=0:
                 raise TypeError('Lags must be a strictly positive number!')
@@ -124,7 +143,132 @@ class FamaMacBeth(object):
         self._Y = Y
         self._X = X
         self._const = add_constant           
-            
+
+    
+    def summary(self):
+        """
+        Prints nice output of the regression
+        """
+        if self.output is None:
+            raise ValueError("You need to fit the model first!")
+        
+        columns = shutil.get_terminal_size().columns
+        title = "Fama-MacBeth estimation results"
+        print(title.center(columns))
+        print('='*columns)
+        tab = 4
+        smallCol = columns/2-tab
+        
+        # getting and printing small stats
+        sts = self._smallStats()
+        for i in range(3):
+            item1 = sts[i]
+            item2 = sts[i+3]
+            print(self._cellStr(item1[0], item1[1], smallCol) + tab*' '
+                  + self._cellStr(item2[0], item2[1], smallCol))
+        
+        print('='*columns)
+        ocl = (columns)/4-tab
+        # print output
+        if self.xLabel is None:
+            # use simply the values of the  output
+            xLabels = self.output.index.tolist()
+            if self._const:
+                xLabels[0] = 'Constant'
+        else:
+            xLabels = self.xLabel
+            if self._const:
+                xLabels = ['Constant',]+xLabels
+        self._tableOutput(self.output.values, xLabels, 
+                               list(np.ones(len(xLabels))), tab, ocl)
+        print('='*columns)
+    
+    
+    def _cellStr(self, cellName, cellContent, length):
+        resLen = int(length - len(cellName) - len(cellContent))
+        if cellName !='':
+            cellName = cellName+':'     
+            if resLen<0:
+                return cellName+' '+cellContent
+            else:
+                return cellName+' '*resLen + cellContent
+        
+        else:
+            return ' '*int(length)+' '
+    
+    
+    def _tableOutput(self, output, rowNames, reps, tab, ocl):
+        columns = shutil.get_terminal_size().columns
+        poq = np.cumsum(reps)
+        pointer = 0
+        counter = 0
+        print(int(ocl)*' '+tab*' '
+              + ' '*int(ocl-len('Estimate'))+'Estimate' +tab*' '
+              + ' '*int(ocl-len('Std. Error'))+'Std. Error'+tab*' '
+              + ' '*int(ocl-len('t-stat'))+'t-stat'
+              )
+        print('-'*columns)
+        # remove names with zero reps       
+        # build the table
+        if np.shape(output)[1]>1:
+            for i in range(len(output)):
+                item = np.round(output[i], decimals = 3)
+                # creating name
+                if i>= poq[pointer]: 
+                    pointer = pointer+1
+                    while reps[pointer] == 0:
+                        pointer = pointer+1
+                    
+                    if reps[pointer]>1:
+                        counter = counter+1
+                    else:
+                        counter = 0
+                
+                elif counter >0:
+                    counter = counter+1
+                
+                if counter == 0:
+                    rowName = rowNames[pointer]
+                else:
+                    rowName = rowNames[pointer]+'['+str(counter)+']'
+                
+                tabLenName = ' '*int(ocl-len(str(rowName)))
+                # putting the values
+                est = str(item[0])
+                se = str(item[1])
+                tstat = str(item[2])
+                print(str(rowName)+tabLenName+tab*' '
+                      +' '*int(ocl-len(est)) + est+ tab*' '
+                      +' '*int(ocl-len(se)) + se+tab*' '
+                      +' '*int(ocl-len(tstat)) + tstat)
+        else:
+            tabLenName = ' '*int(ocl-len(str(rowName)))
+            # putting the values
+            est = str(output[0])
+            se = str(output[1])
+            tstat = str(output[2])
+            print(str(rowName)+tabLenName+tab*' '
+                  +' '*int(ocl-len(est)) + est+ tab*' '
+                  +' '*int(ocl-len(se)) + se+tab*' '
+                  +' '*int(ocl-len(tstat)) + tstat)
+    
+    
+    def _smallStats(self):
+        sts = []
+        if self._NW:
+            errors = 'Newey-West'
+        else:
+            errors = 'White'
+        
+        sts.append(['Dep Variable', str(self.yLabel)])
+        sts.append(['Errors', errors])
+        now = pd.to_datetime('today')
+        sts.append(['Date', now.strftime("%a, %b %d %Y")])
+        sts.append(['Time', now.strftime("%H:%M:%S")])        
+        sts.append(['Num obs T', str(self._T)])
+        sts.append(['Num obs N', str(self._N)])
+        return sts   
+        
      
     def _quickOLS(self, Y, X, add_constant = True):
         """
